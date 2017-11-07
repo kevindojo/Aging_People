8611016396078348c23c3980b4b1ccaf4fe03854
KevinPham <kevinpham15@gmail.com>
2017-11-06 17:39:38 -0800

    first commit

---
+++ AgingPeople/AgingPeople/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  AgingPeople
+//
+//  Created by Kevin Pham on 11/6/17.
+//  Copyright © 2017 Kevin Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let nameBank = [
+        ("Captain America"), ("Iron Man"), ("Hulk"), ("Spiderman"), ("Hawk Eye"), ("Ant Man"), ("Black Widow"), ("Wasp"), ("Black Panther")
+    ]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        ageNum = Int(arc4random_uniform(90)+5)
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (nameBank.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = nameBank[indexPath.row]
+        return cell
+    }
+}
